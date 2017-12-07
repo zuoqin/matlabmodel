38d783aecc020bbe70825e2dcd82d344a09324dc
Alexey Zorchenkov <zuoqinr@163.com>
2017-12-07 16:39:39 +0300

    Correcting script

--- model20171201.m
+++ model20171201.m
@@ -1,26 +1,27 @@
-﻿filename = 'E:/DEV/matlab/sberpb/Modelling20171201.xlsx';
+﻿lastwarn('');
+filename = 'E:/DEV/matlab/sberpb/Modelling20171205.xlsx';
 sheet = 1;
-xlRange = 'H3:I519';
+xlRange = 'H3:I377';
 
 subsetA = xlsread(filename,sheet,xlRange);
 
-xlRange = 'AG3:AG519';
+xlRange = 'AG3:AG377';
 colAG = xlsread(filename,sheet,xlRange);
 
-xlRange = 'A3:A519';
+xlRange = 'A3:A377';
 dates = xlsread(filename,sheet,xlRange);
 
 
 [m,n] = size(subsetA);
 
-logs = zeros(m);
+logs = zeros(m,1);
 
 for k = 3:m
   logs(k) =  log(subsetA(k,2)/subsetA(k-1,2));
 end
 
 
-colK = zeros(m);
+colK = zeros(m,1);
 for k = 52:m
   if k == 52
     start = 2;
@@ -30,19 +31,19 @@ for k = 52:m
   colK(k) =  std(logs (start:k))*sqrt(52);
 end
 
-colL = zeros(m);
+colL = zeros(m,1);
 for k = 52:m
   colL(k) =  subsetA(k,1)/subsetA(k,2) - 1.0;
 end
 
-colM = zeros(m);
+colM = zeros(m,1);
 for k = 52:m
   colM(k) =  colL(k)/colK(k);
 end
 
 
-colN = zeros(m);
-colO = zeros(m);
+colN = zeros(m,1);
+colO = zeros(m,1);
 for k = 64:m
   colN(k) =  mean(colM (k-12:k));
   colO(k) =  std(colM (k-12:k));
@@ -55,48 +56,45 @@ p3 = 0.3;
 p4 = 2;
 p5 = 3;
 
-incase = 1;
-casesmdl = zeros(184680,1);  %15*6*18*6*19 = 184680
-
-for p1 = 1:0.2:3  %15 cases
-  for p2 = 3.5:0.25:5   %6 cases
-    for p3 = 0.1:0.05:1  %18 cases
-      for p4 = 1:1:6   %6 cases
-        for p5 = 0.2:0.2:4  %19 cases
+incase = 0;
+casesmdl = zeros(175560,7);  %11*7*19*6*20 = 175560
+% datetime('now')
 
 
+for p1 = 1:0.2:3            %11 cases
+  for p2 = 3.5:0.25:5       %7 cases
+    for p3 = 0.1:0.05:1     %19 cases
+      for p4 = 1:1:6        %6 cases
+        for p5 = 0.2:0.2:4  %20 cases
+          found = 0;
+          incase = incase + 1;
 
-          colP = zeros(m);
-          colQ = zeros(m);
+          colP = zeros(m,1);
+          colQ = zeros(m,1);
           for k = 63:m
             colP(k) =  colN(k) + p1 * colO(k);
             colQ(k) =  colN(k) - p1 * colO(k);
           end
 
-          colY = zeros(m);
+          colY = zeros(m,1);
           for k = 68:m
             colY(k) = -colL(k)/p5;
           end
 
+          colR  = zeros(m,1);
+          colS  = zeros(m,1);
+          colT  = zeros(m,1);
+          colU  = zeros(m,1);
+          colV  = zeros(m,1);
+          colZ  = zeros(m,1);
+          colAA = zeros(m,1);
 
-          colR  = zeros(m);
-          colS  = zeros(m);
-          colT  = zeros(m);
-          colU  = zeros(m);
-          colV  = zeros(m);
-          colZ  = zeros(m);
-          colAA = zeros(m);
-
-          colAB = zeros(m);
-          colAD = zeros(m);
-          colAE = zeros(m);
-          colAF = zeros(m);
+          colAB = zeros(m,1);
+          colAD = zeros(m,1);
+          colAE = zeros(m,1);
+          colAF = zeros(m,1);
 
           for k = 65:m
-
-
-
-
             colAA(k) = sum(colZ (68:k-1));
             colS(k) =  sum(colR (64:k-1)) + colAA(k);
 
@@ -131,6 +129,10 @@ for p1 = 1:0.2:3  %15 cases
             colT(k) = abs(colR (k)) * 0.002 * subsetA(k,2);
             colV(k) = sum(colU(68:k)) - sum(colT(68:k));
 
+            if colV(k) ~= 0
+            	found = 1;
+            end
+
 
 
 
@@ -139,7 +141,6 @@ for p1 = 1:0.2:3  %15 cases
             else
               colAD(k) = colAD(k-1);
             end
-            
             
             if colS(k) == 0
               if colR(k) == 1
@@ -191,23 +192,43 @@ for p1 = 1:0.2:3  %15 cases
 
           end
 
-          try
-            mdl = fitlm(colV(80:517),dates(80:517));
-          catch
+          if found == 1
+            mdl = fitlm(colV(80:m),dates(80:m));
+            if length(lastwarn()) > 0
+              return
+            else
+              casesmdl(incase,1) = mdl.Rsquared.Ordinary;
+              casesmdl(incase,2) = colV(m);
 
+              casesmdl(incase,3) = p1;
+              casesmdl(incase,4) = p2;
+              casesmdl(incase,5) = p3;
+              casesmdl(incase,6) = p4;
+              casesmdl(incase,7) = p5;
+
+              %disp(casesmdl(incase,:));
+            end
+          else
+            casesmdl(incase,1) = 0;
+            casesmdl(incase,2) = 0;
+          end
           
 
-          casesmdl(incase,1) = mdl.Rsquared.Ordinary;
-          incase = incase + 1;
-
-          if mod(incase,10000) == 0
+          if mod(incase,1000) == 0
+            datetime('now')
             incase = incase
-            return
           end
+
+          
         end
       end
     end
   end
 end
+% datetime('now')
 
 
+B = sortrows(casesmdl,1);
+C = B(170000:175560,1:7);
+D = sortrows(C,2);
+dlmwrite('e:/data/exp.txt',D);
